453ee610e7b5790a14ff0011e683bdd77d271b1d
Leandro Ribeiro <lribeiro19@msn.com>
2023-12-25 20:14:16 -0500

    More setup, lazy.nvim and kanagawa

--- init.lua
+++ init.lua
@@ -20,3 +20,24 @@ vim.opt.inccommand = "split"
 vim.opt.ignorecase = true
 
 vim.opt.termguicolors = true
+
+-- Set up lazy.nvim
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    "rebelot/kanagawa.nvim"
+})
+
+vim.cmd.colorscheme("kanagawa-wave")
