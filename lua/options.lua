e99dd9bde36dddbd7611d94c8643719dafa91d95
Leandro Ribeiro <lribeiro19@msn.com>
2023-12-29 14:53:50 -0500

    context, the rest of the setup

--- lua/options.lua
+++ lua/options.lua
@@ -1,3 +1,8 @@
+vim.g.mapleader = " "
+
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+
 vim.opt.number = true
 vim.opt.relativenumber = true
 
@@ -19,9 +24,43 @@ vim.opt.virtualedit = "block"
 vim.opt.inccommand = "split"
 
 vim.opt.ignorecase = true
+vim.o.smartcase = true
 
 vim.opt.termguicolors = true
 
 vim.opt.hlsearch = false
 
-vim.g.mapleader = " "
+vim.wo.signcolumn = 'yes'
+
+vim.o.completeopt = 'menuone,noselect'
+
+vim.o.breakindent = true
+
+vim.o.undofile = true
+
+-- [[ Basic Keymaps ]]
+
+-- Keymaps for better default experience
+-- See `:help vim.keymap.set()`
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
