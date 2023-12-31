8d03beee2823d94f0d8dcb9fe7bc3cbdc072d4fa
Leandro Ribeiro <lribeiro19@msn.com>
2023-12-30 20:22:46 -0500

    Formatting

--- after/plugin/which-key.lua
+++ after/plugin/which-key.lua
@@ -15,4 +15,3 @@ require("which-key").register({
   ["<leader>"] = { name = "VISUAL <leader>" },
   ["<leader>h"] = { "Git [H]unk" },
 }, { mode = "v" })
-
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -12,8 +12,8 @@ vim.opt.splitright = true
 vim.opt.wrap = false
 
 vim.opt.expandtab = true
-vim.opt.tabstop=4
-vim.opt.shiftwidth=4
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 
 -- vim.opt.clipboard = "unnamedplus"
 
